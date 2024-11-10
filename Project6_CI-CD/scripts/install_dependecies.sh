ace36d619205aa55f49ff12efb09478c19c40d4e
Simeon Cargile <cargileaws@gmail.com>
2024-11-10 12:39:57 -0500

    modify scripts

--- Project6_CI-CD/scripts/install_dependecies.sh
+++ Project6_CI-CD/scripts/install_dependecies.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-yum install -y nginx
+sudo yum -y update
+sudo yum -y install nginx
+sudo yum -y install ruby
+sudo yum -y install wget
+cd /home/ec2-user
+wget https://aws-codedeploy-us-east-1.s3.amazonaws.com/latest/install
+sudo chmod +x ./install
+sudo ./install auto
 rm -f /usr/share/nginx/html/index.html
